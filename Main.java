d88e638d253191d767e507d652aaf5d47ba6ff84
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-04-04 21:37:32 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class pengguna {
+    protected int id;
+    protected String nama;
+    protected String password;
+
+    public pengguna(int id, String nama, String password) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
